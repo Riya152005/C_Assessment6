15007a1e8ef154d2c7ce682c15f9c072c2b9a52c
Riya152005 <riyarathna1507@gmail.com>
2026-06-13 21:08:03 +0530

    Create C_Programming_19.c

---
+++ C_Programming_19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n, x, i, c = 0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+    x = (n / 10) % 100;  
+    for(i = 1; i <= x; i++)
+    {
+        if(x % i == 0)
+            c++;
+    }
+    if(c == 2)
+        printf("Prime");
+    else
+        printf("Not Prime");
+}
